0ad3ace76fd0c9744059d8b4a0d01269a1d3d5bf
akarshxydv <akarshxydv@gmail.com>
2023-02-08 20:45:21 +0530

    Starting some change

--- app/src/main/java/com/example/homedoor/MainActivity.kt
+++ app/src/main/java/com/example/homedoor/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.homedoor
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         lgsub.setOnClickListener(){
-
+            val email=lgemil.text.toString().trim()
+            val pass=lgpas.text.toString().trim()
+            if(email.isEmpty() || pass.isEmpty()){
+                Toast.makeText(this,"Please Enter Email and Password",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            Toast.makeText(this,"Door is open",Toast.LENGTH_SHORT).show()
             val intent = Intent(this,MainActivity2:: class.java)
             intent.putExtra("data",lgemil.text.toString())
             startActivity(intent)
